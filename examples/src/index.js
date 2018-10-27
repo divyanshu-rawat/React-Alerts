b233b4cc5af90b63c029069dde8c96f7df76f8e5
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-10-28 02:52:47 +0530

    examples added

--- examples/src/index.js
+++ examples/src/index.js
@@ -13,9 +13,13 @@ const options = {
 const App = () => (
 	<div>
 	    <Alert margin = "10"/>
-	    <Alert type = "danger"  margin = "10"  message = "error! occured somewhere." />
-	    <Alert type = "warning" margin = "10"  message = "Hurry! check your dependencies!"/>
-	    <Alert {...options} />
+	    <Alert type = "danger"  	margin = "10"    message = "Danger! This alert box indicates a dangerous or potentially negative action." />
+	    <Alert type = "warning" 	margin = "10"    message = "Warning! This alert box indicates a warning that might need attention."/>
+	    <Alert type = "secondary"   margin = "10"  	 message = "Secondary! This alert box indicates a less important action." />
+	    <Alert type = "info" 		margin = "10"    message = "Info! This alert box indicates a neutral informative change or action." />
+	    <Alert type = "success" 	margin = "10"    message = "Success! This alert box indicates a successful or positive action." />
+		<Alert type = "light" 		margin = "10"    message = "Light! Light grey alert box." />
+		<Alert message = "Custom! modify it by passing optionals" {...options}/>
     </div>
 );
 render(<App />, document.getElementById("root"));
