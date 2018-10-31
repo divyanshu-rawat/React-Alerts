9d3b554a87bdb3189390beee9887bf2106f5c611
Pedro Terriquez <pedroterriquez@hotmail.com>
2018-10-31 11:10:42 -0600

    reafactored-switch-case-alert

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 class Alert extends React.Component{
-	render(){
+	renderAlert(className, divStyle, strongMessage, restMessage) {
+		return(
+			<div className = { className } style = { divStyle }>
+				<a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
+				<strong>{ strongMessage }</strong> { restMessage }
+			</div>
+		);
+	}
 
+	render(){
 		let alert = null;
 		let restMessage;
 		let strongMessage;
@@ -17,7 +25,7 @@ class Alert extends React.Component{
 		}else{
 			strongMessage  = "Success!"
 			restMessage    = "Component Imported Successfully!!"
-		}	
+		}
 
 		let divStyle = {
 		  margin: this.props.margin,
@@ -26,91 +34,29 @@ class Alert extends React.Component{
 		  background : this.props.backgroundColor
 		};
 
-		switch (this.props.type) {
-		    case 'danger':
-		         alert = (
-			         <div className="alert alert-danger" style = { divStyle }>
-			          <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					  <strong>{ strongMessage }</strong> { restMessage }
-					</div>
-			      );
-		        break;
-
-		    case 'info':
-		         alert = (
-			         <div className="alert alert-info" style = { divStyle }>
-			          <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					  <strong>{ strongMessage }</strong> { restMessage }
-					</div>
-			      );
-		        break;
-
-		    case 'success':
-		    	 alert = (
-			          <div className="alert alert-success" style = { divStyle }>
-					    <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					    <strong>{ strongMessage }</strong> { restMessage }
-					  </div>
-			      );
-		    	 break;
-
-		   	case 'warning':
-		    	 alert = (
-			          <div className="alert alert-warning" style = { divStyle }>
-					    <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					    <strong>{ strongMessage }</strong> { restMessage }
-					  </div>
-			      );
-		    	 break;
-
-		    case 'secondary':
-		    	 alert = (
-			          <div className="alert alert-secondary" style = { divStyle }>
-					    <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					    <strong>{ strongMessage }</strong> { restMessage }
-					  </div>
-			      );
-		    	 break;
-
-		     case 'light':
-		    	 alert = (
-			          <div className="alert alert-light" style = { divStyle }>
-					    <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					    <strong>{ strongMessage }</strong> { restMessage }
-					  </div>
-			      );
-		    	 break;
-
-		    default:
-		        alert = (
-			          <div className="alert alert-success" style = { divStyle }>
-					    <a href="#" className="close" data-dismiss="alert" aria-label="close">×</a>
-					    <strong>{ strongMessage }</strong> { restMessage }
-					  </div>
-			      );
-		}
+		let className = `alert ${Alert.validTypes[this.props.type].className}`;
 
 		return(
 				<div>
-					{ alert }
+					{ this.renderAlert(className, divStyle, strongMessage, restMessage) }
 				</div>
 		)
 	}
 }
 
-Alert.validTypes = [
-  'default',
-  'danger',
-  'warning',
-  'secondary',
-  'info',
-  'success',
-  'light',
-  'custom'
-];
+Alert.validTypes = {
+	default: { className: 'alert-success' },
+	danger: { className: 'alert-danger' },
+	warning: { className: 'alert-warning' },
+	secondary: { className: 'alert-secondary' },
+	info: { className: 'alert-info' },
+	success: { className: 'alert-success' },
+	light: { className: 'alert-light' },
+	custom: { className: 'alert-custom' }
+};
 
 Alert.propTypes = {
-  type: PropTypes.oneOf(Alert.validTypes),
+  type: PropTypes.oneOf(Object.keys(Alert.validTypes)),
   message: PropTypes.string,
   margin: PropTypes.string,
   width: PropTypes.string,
@@ -123,7 +69,8 @@ Alert.defaultProps = {
   width: null,
   fontColor: null,
   backgroundColor: null,
-  message: null
+  message: null,
+  type: 'default'
 };
 
 export default Alert;
